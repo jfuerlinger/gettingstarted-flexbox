fb3136a705a19d540ff38fba073bf090809116fe
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2018-05-21 09:14:09 +0200

    Finished "Output Management"

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and chech the console';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
     
-    console.log(Data);
-
     return element;
 }
 
